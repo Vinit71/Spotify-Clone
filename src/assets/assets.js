a4fd97abc22c82064d0d28e8e472af327f23df5e
Vinit <vinitsk99@gmail.com>
2024-05-29 09:57:34 +0530

    new Assets

---
+++ src/assets/assets.js
@@ -0,0 +1,120 @@
+import Arijit from './arijit.jpg'
+import KK from './KK.jpg'
+import Eminem from './eminem.jpg'
+import Lofi from './lofi.jpg'
+import Music1 from './music1.jpg'
+import Song1Image from './Song1Image.jpg'
+import Top50 from './top50.jpg'
+import Song1 from './song1.mp3'
+import Home from './home.svg'
+import Search from './search.svg'
+// import Arijit from './arijit.jpg'
+export const assets = {
+    Arijit,
+    KK,
+    Eminem,
+    Lofi,
+    Music1,
+    Song1,
+    Top50,
+    Home,
+    Search
+}
+
+export const albumData = [
+    {
+        id: 1,
+        name: "Top 50 India",
+        image: Top50,
+        disc: "Most Listened Songs this Week",
+        bgColor: "#742a2a"
+    },
+    {
+        id: 2,
+        name: "Arijit Singh",
+        image: Arijit,
+        disc: "Best of Arijit Singh",
+        bgColor: "#742a2a"
+    },
+    {
+        id: 3,
+        name: "KK",
+        image: KK,
+        disc: "The KK Era through back",
+        bgColor: "#742a2a"
+    },
+    {
+        id: 4,
+        name: "Eninem",
+        image: Eminem,
+        disc: "Fuel your Workouts",
+        bgColor: "#742a2a"
+    }
+]
+
+export const songsData = [
+    {
+        id: 1,
+        name: "Song1",
+        image: Music1,
+        file: Song1,
+        desc: "Song1",
+        duration: "3:00"
+    },
+    {
+        id: 2,
+        name: "Song2",
+        image: Music1,
+        file: Song1,
+        desc: "Best",
+        duration: "4:00"
+    },
+    {
+        id: 3,
+        name: "Song3",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+    {
+        id: 4,
+        name: "Song4",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+    {
+        id: 5,
+        name: "Song5",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+    {
+        id: 6,
+        name: "Song6",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+    {
+        id: 7,
+        name: "Song7",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+    {
+        id: 8,
+        name: "Song8",
+        image: Music1,
+        file: Song1,
+        desc: "Above Best",
+        duration: "5:00"
+    },
+]
