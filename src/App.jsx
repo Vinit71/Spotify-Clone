36f09520ff7ccc5646b2b85fd1dc9ebd2cc2c6a0
Vinit <vinitsk99@gmail.com>
2024-05-31 00:13:45 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar.jsx'
+import Player from './Components/Player.jsx'
+import Display from './Components/Display.jsx'
 function App() {
   return (
     <div className='h-screen bg-black'>
-      <div>
+      <div className='h-[91%] flex'>
         <Sidebar />
+        <Display />
       </div>
+        <Player />
     </div>
   )
 }
